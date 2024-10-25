d3c3359e6f542447208fec47f7060e82d90c00c7
huynvebisolvn <huy.nv@japanticket.com>
2024-10-25 10:30:22 +0700

    Update Sub.vue

--- src/components/Sub.vue
+++ src/components/Sub.vue
@@ -38,6 +38,23 @@ export default {
         setCookie('wishList', '')
       }
       setCookie('taskId', params.value.taskId)
+
+      const urlUser = urlParams.get("user")
+      if (urlUser) {
+        myUsersName.value = urlUser
+      }
+
+      const urlSet = urlParams.get("set")
+      if (urlSet) {
+        const wishs = urlSet.split(',')
+        let rs = []
+        for (const wish of wishs) {
+          if (Number(wish)) rs.push(Number(wish))
+        }
+        wishList.value = rs
+        wishListInput.value = rs
+        setCookie('wishList', rs.join(','))
+      }
     }
     const funGetWishParams = () => {
       const wishs = wishListInput.value?.split(',')
@@ -278,6 +295,9 @@ export default {
     const clearChildItem = () => {
       childItems.value = []
     }
+    const saveLink = () => {
+      window.location.href += `&set=${wishList.value}&user=${myUsersName.value}`
+    }
     const setCookie = (cname: string, cvalue: string) => {
       document.cookie = cname + '=' + encodeURIComponent(cvalue) + '; path=/';
     }
@@ -315,6 +335,7 @@ export default {
       funcRemoveWishList,
       getChildItem,
       clearChildItem,
+      saveLink,
       setCookie,
       getCookie,
     }
@@ -346,6 +367,12 @@ export default {
     </button>
     <input class="ml-2 border-2 border-rose-500" v-model="myUsersName" placeholder="Nhập tên user" @change="setCookie('myUsersName', myUsersName)"/>
     <input class="ml-2 border-2 border-teal-500" v-model="wishListInput" placeholder="Nhập wish list" @change="funGetWishParams"/>
+    <button
+      type="button" class="ml-2 px-5 text-white bg-green-700 hover:bg-green-800 focus:outline-none font-medium rounded-lg px-1 text-center dark:bg-green-600 dark:hover:bg-green-700"
+      @click="saveLink"
+      >
+      Save
+    </button>
     <p class="break-all text-rose-500"> {{ usersTaskMap.get(myUsersName)  }}</p>
     <p class="break-all text-teal-500">{{ wishList }}</p>
     <label v-if="isError" class="text-3xl text-red-500">Hết hạn rồi, đăng nhập lại!</label>
